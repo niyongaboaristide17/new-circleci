e94df005752dc86bf34f7668895acf0fbe49e425
niyongaboaristide17 <niyongaboaristide17@gmail.com>
2022-03-05 04:43:27 +0200

    test: update test.js

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,12 @@
 import { expect, request, use } from 'chai'
 import chaiHttp from 'chai-http'
-import { testing } from '../src/controllers/test.controller'
+import { testing, testing2 } from '../src/controllers/test.controller'
 
 describe('TEST', ()=>{
     it('SHOULD RETURN TRUE', ()=>{
         expect(testing()).to.equal(true)
+    })
+    it('SHOULD RETURN 2ND TRUE', ()=>{
+        expect(testing2()).to.equal(true)
     })
 })
